3d12315e178810c7a4fa27e0ad01610d380351f0
Jarek Decowski <jaroslaw.decowski@gmail.com>
2020-11-09 15:01:50 +0100

    added custom bootstrap testimonials, faq, cta section, icons

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+//HAMBURGER MENU ON CLICK
+
 const btn = document.querySelector('.hamburger-menu');
 const nav = document.querySelector('nav');
 
@@ -6,3 +8,29 @@ const nav = document.querySelector('nav');
 btn.addEventListener('click', function (){
     nav.classList.toggle('nav-active');
 });
+
+// BOOTSTRAP CAROUSEL
+
+$('.carousel').carousel({
+    interval: 3500
+})
+
+//SHOW-BUTTON ON HOVER OVER IMG
+
+// const img1 = document.querySelector('.cardimg1');
+// const img2 = document.querySelector('.cardimg2');
+// const btnAddCart1 = document.querySelector('.btn-hide');
+// const outside = document.querySelector('.product-cards');
+
+
+// img1.addEventListener('mouseover', function(){
+//     btnAddCart1.classList.toggle('btnAddCart');
+     
+// });
+
+// outside.addEventListener('mouseover', function(){
+//     btnAddCart1.classList.remove('btnAddCart');
+//     btnAddCart1.classList.add('btn-hide');
+// });
+  
+
